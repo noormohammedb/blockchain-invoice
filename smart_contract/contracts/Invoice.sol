f6597184018dd861e169a53a01ec6b453e5cd3e8
Noor Mohammed B <noormohammedb@protonmail.com>
2022-11-14 12:51:12 +0400

    Minor fix
    removed old reference code unwanted comments

--- smart_contract/contracts/Invoice.sol
+++ smart_contract/contracts/Invoice.sol
@@ -22,31 +22,21 @@ contract Invoice {
         uint256 pro_qty;
     }
 
-    INVOICE_METADAT[] globalInvoice;
-    uint256 globalCounter = 0;
+    INVOICE_METADAT[] public globalInvoice;
+    uint256 public globalCounter = 0;
 
     /* user <=> invoice */
-    // mapping(address => INVOICE_METADAT[]) userToInvoiceNumberArray;
-    mapping(address => uint256[]) userToInvoiceNumberArray;
-    mapping(uint256 => address) invoiceNoToAddress;
+    mapping(address => uint256[]) public userToInvoiceNumberArray;
+    mapping(uint256 => address) public invoiceNoToAddress;
 
     /* invoice => [product] => invoice data */
-    mapping(uint256 => PRODUCT_DATA[]) invoiceProducts;
+    mapping(uint256 => PRODUCT_DATA[]) public invoiceProducts;
 
     function addInvoice(
         string memory prod_name,
         uint256 prod_price,
         uint256 prod_qty
     ) external returns (uint256) {
-        globalCounter++;
-
-        // PRODUCT_DATA memory myProduct = PRODUCT_DATA({
-        //     pro_id: 0,
-        //     pro_name: prod_name,
-        //     pro_price: prod_price,
-        //     pro_qty: prod_qty
-        // });
-
         invoiceProducts[globalCounter].push(
             PRODUCT_DATA({
                 pro_id: 0,
@@ -55,12 +45,6 @@ contract Invoice {
                 pro_qty: prod_qty
             })
         );
-
-        // INVOICE_METADAT memory myInvoice = INVOICE_METADAT({
-        //     inv_no: globalCounter,
-        //     inv_date: block.timestamp,
-        //     inv_total_amt: prod_price * prod_qty
-        // });
 
         globalInvoice.push(
             INVOICE_METADAT({
@@ -74,33 +58,8 @@ contract Invoice {
 
         invoiceNoToAddress[globalCounter] = msg.sender;
 
+        globalCounter++;
+
         return 0;
     }
 }
-
-/*
-pragma solidity >=0.7.0 <0.9.0;
-
-contract GoldSouk {
-    struct INVOICE_HEAD {
-        uint256 date;
-        uint256 id;
-        uint256 totalPrice;
-    }
-
-    struct INVOICE_BODY {
-        uint256 product_id;
-        string prod_name;
-        uint256 price;
-        uint256 quantity;
-    }
-
-    mapping(address => uint256[]) userTobill;
-
-    uint256 globalID = 0;
-
-    // function addAInvoice(){
-
-    // }
-}
-*/
