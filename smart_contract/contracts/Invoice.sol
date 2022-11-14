feef654b6c612da4b1db1e2909622dc5fdcb89f5
Noor Mohammed B <noormohammedb@protonmail.com>
2022-11-14 16:48:32 +0400

    Add invoice and take invoice data
    add multiple products into a invoice
    get all data of a invoice

--- smart_contract/contracts/Invoice.sol
+++ smart_contract/contracts/Invoice.sol
@@ -16,41 +16,58 @@ contract Invoice {
     }
 
     struct PRODUCT_DATA {
-        uint256 pro_id;
         string pro_name;
         uint256 pro_price;
         uint256 pro_qty;
     }
 
+    struct FULL_INVOICE {
+        INVOICE_METADAT header;
+        PRODUCT_DATA[] products;
+    }
+
+    // FULL_INVOICE[] globalInvoice2;
     INVOICE_METADAT[] public globalInvoice;
     uint256 public globalCounter = 0;
 
     /* user <=> invoice */
-    mapping(address => uint256[]) public userToInvoiceNumberArray;
-    mapping(uint256 => address) public invoiceNoToAddress;
+    mapping(address => uint256[]) userToInvoiceNumberArray;
+    mapping(uint256 => address) invoiceNoToAddress;
 
     /* invoice => [product] => invoice data */
-    mapping(uint256 => PRODUCT_DATA[]) public invoiceProducts;
+    mapping(uint256 => PRODUCT_DATA[]) invoiceProducts;
 
-    function addInvoice(
-        string memory prod_name,
-        uint256 prod_price,
-        uint256 prod_qty
-    ) external returns (uint256) {
-        invoiceProducts[globalCounter].push(
-            PRODUCT_DATA({
-                pro_id: 0,
-                pro_name: prod_name,
-                pro_price: prod_price,
-                pro_qty: prod_qty
-            })
-        );
+    function getInvoice(uint256 invoiceNumber)
+        external
+        view
+        returns (FULL_INVOICE memory)
+    // returns (INVOICE_METADAT memory, PRODUCT_DATA[] memory)
+    {
+        INVOICE_METADAT memory myInvoice = globalInvoice[invoiceNumber];
+
+        PRODUCT_DATA[] memory myProducts = invoiceProducts[invoiceNumber];
+
+        return FULL_INVOICE(myInvoice, myProducts);
+    }
+
+    function addInvoice(PRODUCT_DATA[] memory prod) external returns (uint256) {
+        uint256 totalPrice = 0;
+        for (uint256 i = 0; i < prod.length; i++) {
+            invoiceProducts[globalCounter].push(
+                PRODUCT_DATA(
+                    prod[i].pro_name,
+                    prod[i].pro_price,
+                    prod[i].pro_qty
+                )
+            );
+            totalPrice += prod[i].pro_price * prod[i].pro_qty;
+        }
 
         globalInvoice.push(
             INVOICE_METADAT({
                 inv_no: globalCounter,
                 inv_date: block.timestamp,
-                inv_total_amt: prod_price * prod_qty
+                inv_total_amt: totalPrice
             })
         );
 
@@ -60,6 +77,48 @@ contract Invoice {
 
         globalCounter++;
 
-        return 0;
+        return globalCounter;
+
+        // PRODUCT_DATA[] memory myProducts;
+        // myProducts[0] = PRODUCT_DATA({
+        //     pro_id: 0,
+        //     pro_name: prod_name,
+        //     pro_price: prod_price,
+        //     pro_qty: prod_qty
+        // });
+
+        // PRODUCT_DATA[1] memory myProducts = [
+        //     PRODUCT_DATA({
+        //         pro_id: 0,
+        //         pro_name: prod_name,
+        //         pro_price: prod_price,
+        //         pro_qty: prod_qty
+        //     })
+        // ];
+
+        // myProducts[0] = PRODUCT_DATA({
+        //     pro_id: 0,
+        //     pro_name: prod_name,
+        //     pro_price: prod_price,
+        //     pro_qty: prod_qty
+        // });
+
+        // myProducts.push();
+
+        // globalInvoice2.push(INVOICE_METADAT, PRODUCT_DATA);
+
+        // globalInvoice2.push(
+        //     INVOICE_METADAT({
+        //         inv_no: globalCounter,
+        //         inv_date: block.timestamp,
+        //         inv_total_amt: prod_price * prod_qty
+        //     }),
+        //     PRODUCT_DATA({
+        //         pro_id: 0,
+        //         pro_name: prod_name,
+        //         pro_price: prod_price,
+        //         pro_qty: prod_qty
+        //     })
+        // );
     }
 }
